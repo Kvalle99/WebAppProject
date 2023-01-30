f391e4adf64557fc4a781fe1179e4bdbd9fa59a7
Kvalle99 <sebastian.kvalden@gmail.com>
2023-01-30 11:05:00 +0100

    heh

--- server/src/model/trip.ts
+++ server/src/model/trip.ts
@@ -1,17 +1,33 @@
 import { Activity } from "./activity";
+import { Date } from "./Date";
 
 export class Trip {
-    destination : string;
-    startDate : string;
-    endDate : string;
-    hotel : string;
-    activities : Activity[];
+  destination: string;
+  startDate: Date;
+  endDate: Date;
+  hotel: string;
+  activities: Activity[];
 
-    constructor(destination : string, startDate : string, endDate : string, hotel : string, activities : Activity[]) {
-        this.destination = destination;
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.hotel = hotel;
-        this.activities = activities;
-    }
+  constructor(
+    destination: string,
+    startDate: Date,
+    endDate: Date,
+    hotel: string,
+    activities: Activity[]
+  ) {
+    this.destination = destination;
+    this.startDate = startDate;
+    this.endDate = endDate;
+    this.hotel = hotel;
+    this.activities = activities;
+  }
+
+  removeActivity(activity: Activity) {
+    var index = this.activities.indexOf(activity);
+    this.activities.splice(index, 1);
+  }
+
+  addActivity(activity: Activity) {
+    this.activities.push(activity);
+  }
 }
--- server/src/service/trip.service.ts
+++ server/src/service/trip.service.ts
@@ -1,9 +1,17 @@
 import { Activity } from "../model/activity";
+import { Date } from "../model/Date";
 import { Trip } from "../model/trip";
 import { ITripService } from "./itripservice";
 
 export class TripService implements ITripService {
-  trip = new Trip("new york", "startdate", "enddate", "hotel", []);
+  //will be collected from database later
+  trip = new Trip(
+    "new york",
+    new Date(BigInt(2023), BigInt(11), BigInt(11)),
+    new Date(BigInt(2023), BigInt(11), BigInt(19)),
+    "hotel",
+    []
+  );
 
   constructor() {}
 
@@ -13,5 +21,25 @@ export class TripService implements ITripService {
 
   async changeHotel(hotel: string) {
     this.trip.hotel = hotel;
+  }
+
+  async changeStartDate(newDate: Date) {
+    this.trip.startDate = newDate;
+  }
+
+  async changeEndDate(newDate: Date) {
+    this.trip.endDate = newDate;
+  }
+
+  async getActivities(): Promise<Activity[]> {
+    return this.trip.activities;
+  }
+
+  async addActivities(activity: Activity) {
+    this.trip.addActivity(activity);
+  }
+
+  async removeActivities(activity: Activity) {
+    this.trip.removeActivity(activity);
   }
 }
