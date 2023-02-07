3a88ffd3929679749de6ddbdfb7879a67d0c92a1
florett1 <70637056+florett1@users.noreply.github.com>
2023-02-07 14:49:40 +0100

    Test component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Hejhej from './components/hejhej/hejhej';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Hejhej></Hejhej>
     </div>
   );
 }
