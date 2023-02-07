59f73aa6217995d1a82414edd744c3b09e83abb9
florett1 <70637056+florett1@users.noreply.github.com>
2023-02-07 09:56:27 +0100

    install react

--- server/src/start.ts
+++ server/src/start.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { ActivityRouter } from "./router/activity.router";
 import { TripRouter } from "./router/trip.router";
+import cors from "cors";
 
 export const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use("/hotel", TripRouter);
 app.use("/activity", ActivityRouter);
