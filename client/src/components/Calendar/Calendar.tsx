f2e60603e891d214a045af70e0e426d69a4a79fa
Kvalle99 <sebastian.kvalden@gmail.com>
2023-02-08 18:00:40 +0100

    added the navbar

--- client/src/components/Calendar/Calendar.tsx
+++ client/src/components/Calendar/Calendar.tsx
@@ -2,16 +2,34 @@ import React, { Component, FC, useState } from "react";
 import "./Calendar.module.css";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function CalendarComponent() {
-  const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
+  const [selectedStartDate, setSelectedStartDate] = useState<Date | null>(
+    new Date()
+  );
+  const [selectedEndDate, setSelectedEndDate] = useState<Date | null>(
+    new Date()
+  );
 
   return (
     <div className="CalenderComponent">
-      <DatePicker
-        selected={selectedDate}
-        onChange={(date) => date && setSelectedDate(date)}
-      />
+      <div className="container">
+        <div className="row">
+          <div className="col-2">
+            <DatePicker
+              selected={selectedStartDate}
+              onChange={(date) => date && setSelectedStartDate(date)}
+            />
+          </div>
+          <div className="col-2">
+            <DatePicker
+              selected={selectedEndDate}
+              onChange={(date) => date && setSelectedEndDate(date)}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import CalendarComponent from "./components/Calendar/Calendar";
+import Navbar from "./components/Navbar/Navbar";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -11,6 +12,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
+    <Navbar />
     <CalendarComponent />
   </React.StrictMode>
 );
